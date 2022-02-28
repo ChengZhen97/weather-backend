34328359d6577b9afeeaed849e3d3a0888389c4e
ChengZhen97 <90570127+ChengZhen97@users.noreply.github.com>
2022-02-27 22:24:00 -0800

    secure other two endpoints (#3)

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.get('/v1/weather', (req, res) => {
+app.get('/v1/weather', authJWT, (req, res) => {
+
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=a99f1dbe7d7e7d5b6ce85970a31da042'
   request(url, (error, response, body)=>{
     console.log(body);
@@ -20,7 +21,8 @@ app.get('/v1/weather', (req, res) => {
 
 })
 
-app.get('/v1/hello', (req, res) => {
+app.get('/v1/hello', authJWT, (req, res) => {
+
   const sayHello = "Hello world";
   res.json({sayHello});
 })
@@ -39,5 +41,16 @@ app.post('/v1/auth', (req, res) =>{
   //res.sendStatus(200);
   res.json({requestBody : req.body})
 });
+
+
+const authJWT = (req, res, next) => {
+  const headerInfo = req.headers;
+
+  if(headerInfo != "eyJwYXNzd29yZCI6IlBhc3N3b3JkIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0NDE5MjE4OCwiZXhwIjoxNjQ0MTk1Nzg4fQ.YP0OKHcljuZf6X9V8tMimfcOrzTc63-ia9rRrgOA0eA"){
+    return res.sendStatus(403);
+  }
+
+  next();
+}
 
 
